7a1a64767dffd1ab1a1e68696c53ae666d1a13f4
Lalawi21 <lealawitteferi@gmail.com>
2025-04-07 20:58:35 +0300

    smallest number in the array cpp file

---
+++ smallest1.cpp
@@ -0,0 +1,23 @@
+##Assignment 1
+#nclude<iostream>
+using namespace std;
+
+int findsmallest(int arr[], int size) {
+    int minNumber = arr[0]; // Assume the first element is the largest
+    
+    for (int i = 1; i < size; i++) {
+        if (arr[i] <minNumber) {
+            minNumber = arr[i]; // Update max if a larger number is found
+        }
+    }
+    return minNumber;
+} 
+int main() {
+    int arr[] = {120, 45, 67, 89, 34, 23, 90, 11}; 
+    int size = sizeof(arr) / sizeof(arr[0]); // Calculate array size
+
+    int smallest = findsmallest(arr, size);
+    cout << "The smallest number in the array is: " << smallest<< endl;
+
+    return 0;
+}
